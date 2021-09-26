ddf6cdf5b37d91fa190c8279d0acd2f585994bbb
HemaHSR <hemavatihsr@gmail.com>
2021-09-26 16:36:16 +0530

    String Concatination

--- testPython.py
+++ testPython.py
@@ -2,4 +2,6 @@ x = 2 + 3000
 y = 300 + 2.1
 print(int(x) + int(y))
 print(x + y)
-print("It all workes C:\xampp\htdocs\projects\PyShop\testPython.py")
+print("It all workes C:\\xampp\\htdocs\\projects\\PyShop\\testPython.py")
+xy = 'aB' + 'xx'
+print(xy)
